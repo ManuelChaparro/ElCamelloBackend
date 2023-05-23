dda3f933d14c8406fcda63cb0b03479b8d0d1ab6
ManuelChaparro <manuel.chaparro01@uptc.edu.co>
2023-05-23 18:10:45 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -22,6 +22,12 @@ app.get(conn);
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 //routes
 app.use('/api', userRoutes);
